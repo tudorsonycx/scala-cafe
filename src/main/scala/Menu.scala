9f6b22468c522739b6a5efcc31d1b53df1df7c57
tudorsonycx <tudorsonycx@gmail.com>
2025-03-03 15:23:52 +0000

    Override toString method in Menu case class for better string representation and add showMenu method

--- src/main/scala/Menu.scala
+++ src/main/scala/Menu.scala
@@ -1,1 +1,5 @@
-case class Menu private(items: List[Item])
+case class Menu private(items: List[Item]) {
+  override def toString: String = items.mkString("\n")
+
+  def showMenu: String = toString
+}
