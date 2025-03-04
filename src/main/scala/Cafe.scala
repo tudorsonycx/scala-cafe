e018336d9b24d5ac64fd6712a6d943322cc91f73
tudorsonycx <tudorsonycx@gmail.com>
2025-03-04 10:44:14 +0000

    Rename Cafe.CafeMenuError to Cafe.CafeError

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -5,7 +5,7 @@ case class Cafe(name: String, private val menu: Menu) {
 
   var menuWithStock: Map[Item, Int] = menu.items.map(item => (item, item.stock)).toMap
 
-  def getItemCost(item: Item, quantity: Int): Either[Cafe.CafeMenuError, Double] =
+  def getItemCost(item: Item, quantity: Int): Either[Cafe.CafeError, Double] =
     menuWithStock.get(item) match {
       case Some(stock) if stock >= quantity => Right(item.price * quantity)
       case Some(stock) => Left(
@@ -13,7 +13,7 @@ case class Cafe(name: String, private val menu: Menu) {
       case None => Left(Cafe.MenuInvalidItemError(s"${item.name} not found in menu"))
     }
 
-  def updateMenuItem(item: Item, stock: Int): Either[Cafe.CafeMenuError, Map[Item, Int]] = {
+  def updateMenuItem(item: Item, stock: Int): Either[Cafe.CafeError, Map[Item, Int]] = {
     menuWithStock.get(item) match {
       case None => Left(Cafe.MenuInvalidItemError(s"${item.name} not found in menu"))
       case Some(_) if stock >= 0 => Right(menuWithStock + (item -> stock))
@@ -23,9 +23,9 @@ case class Cafe(name: String, private val menu: Menu) {
 }
 
 object Cafe {
-  abstract class CafeMenuError(message: String) extends Exception(message)
+  abstract class CafeError(message: String) extends Exception(message)
 
-  case class MenuInvalidItemError(message: String) extends CafeMenuError(message)
+  case class MenuInvalidItemError(message: String) extends CafeError(message)
 
-  case class MenuInvalidQuantityError(message: String) extends CafeMenuError(message)
+  case class MenuInvalidQuantityError(message: String) extends CafeError(message)
 }
