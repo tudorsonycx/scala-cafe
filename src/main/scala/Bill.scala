2adfc24799760a9c905a3b60a782901a6e5bd948
tudorsonycx <tudorsonycx@gmail.com>
2025-03-05 14:27:39 +0000

    Refactor toString method in Bill class for improved readability

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -7,7 +7,9 @@ case class Bill(customer: Customer, items: Map[Item, Int], serviceCharge: Double
   }
 
   override def toString: String = {
-    val itemDetails = items.map { case (item, quantity) => s"${item.name} x $quantity" }.mkString("\n")
+    val itemDetails = items.map({
+      case (item, quantity) => s"${item.name} x $quantity"
+    }).mkString("\n")
     s"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $total\n" +
       s"Service Charge: $serviceCharge\nTotal: ${total * (1 + serviceCharge)}"
   }
