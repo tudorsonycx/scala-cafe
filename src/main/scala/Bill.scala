08b974d99c90ed996261c3e4290f9d39cbbffeef
tudorsonycx <tudorsonycx@gmail.com>
2025-03-05 01:23:53 +0000

    Refactor Bill class to include items and service charge, and add total calculation and custom toString method

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -1,1 +1,14 @@
-case class Bill(customer: Customer, amount: Double)
+case class Bill(customer: Customer, items: Map[Item, Int], serviceCharge: Double) {
+  def total: Double = {
+    val itemTotal = items.map({
+      case (item, quantity) => item.price * quantity
+    }).sum
+    itemTotal
+  }
+
+  override def toString: String = {
+    val itemDetails = items.map { case (item, quantity) => s"${item.name} x $quantity" }.mkString("\n")
+    s"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $total\n" +
+      s"Service Charge: $serviceCharge\nTotal: ${total * (1 + serviceCharge)}"
+  }
+}
