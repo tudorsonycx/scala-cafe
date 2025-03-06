fa8fafaef8a5f9aa0eac8ed595c1e463b8581d2d
tudorsonycx <tudorsonycx@gmail.com>
2025-03-06 14:37:56 +0000

    Refactor ItemCategory hierarchy to separate Food and Drink categories

--- src/main/scala/Item.scala
+++ src/main/scala/Item.scala
@@ -1,16 +1,21 @@
 sealed trait ItemCategory
 
-case object HotFood extends ItemCategory
+sealed trait Food extends ItemCategory
 
-case object ColdFood extends ItemCategory
+sealed trait Drink extends ItemCategory
 
-case object HotDrink extends ItemCategory
+case object HotFood extends Food
 
-case object ColdDrink extends ItemCategory
+case object ColdFood extends Food
 
-case object AlcoholicDrink extends ItemCategory
+case object PremiumMeal extends Food
 
-case object PremiumMeal extends ItemCategory
+case object HotDrink extends Drink
+
+case object ColdDrink extends Drink
+
+case object AlcoholicDrink extends Drink
+
 
 case class Item(name: String, price: Double, category: ItemCategory, stock: Int = 0) {
   override def toString: String = s"$name ($category) - $price"
