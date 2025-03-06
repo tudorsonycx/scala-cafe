796afdc8ab3f6c61069216387cfbb493d7ccfdc6
tudorsonycx <tudorsonycx@gmail.com>
2025-03-06 10:29:44 +0000

    Refactor placeOrder method in Cafe class to accept item names and improve service charge calculation

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -3,27 +3,34 @@ case class Cafe(name: String, private val menu: Menu) {
 
   def placeOrder(
     customer: Customer,
-    items: Map[Item, Int],
+    items: Map[String, Int],
     addServiceCharge: Boolean = true)
     (customServiceCharge: Option[Double] = None): Either[Cafe.CafeError, Bill] = {
-    val serviceCharge: Either[Cafe.OrderInvalidServiceCharge, Double] = if (addServiceCharge) {
-      customServiceCharge match {
-        case Some(charge) if charge >= 0 => Right(charge)
-        case Some(_) => Left(Cafe.OrderInvalidServiceCharge("Service charge cannot be negative"))
-        case None =>
-          items.keys match {
-            case ik if ik.exists(_.category == PremiumMeal) => Right(0.25)
-            case ik if ik.exists(_.category == HotFood) => Right(0.2)
-            case ik if ik.exists(_.category == ColdFood) => Right(0.1)
-            case _ => Right(0)
-          }
-      }
-    } else Right(0)
-
     items match {
       case i if i.isEmpty => Left(Cafe.OrderInvalidItemList("Order cannot be empty"))
       case _ =>
-        menu.updateMenuAfterOrder(items).flatMap(_ => serviceCharge.map(sc => Bill(customer, items, sc)))
+        menu.updateMenuAfterOrder(items) match {
+          case Left(e) => Left(e)
+          case Right(_) =>
+            val itemsWithStock: Map[Item, Int] = items.map({
+              case (itemName, quantity) => (menu.getItemByName(itemName).get, quantity)
+            })
+            val serviceCharge: Either[Cafe.OrderInvalidServiceCharge, Double] = if (addServiceCharge) {
+              customServiceCharge match {
+                case Some(charge) if charge >= 0 => Right(charge)
+                case Some(_) => Left(Cafe.OrderInvalidServiceCharge("Service charge cannot be negative"))
+                case None =>
+                  itemsWithStock.keys match {
+                    case ik if ik.exists(_.category == PremiumMeal) => Right(0.25)
+                    case ik if ik.exists(_.category == HotFood) => Right(0.2)
+                    case ik if ik.exists(_.category == ColdFood) => Right(0.1)
+                    case _ => Right(0)
+                  }
+              }
+            } else Right(0)
+
+            serviceCharge.map(sc => Bill(customer, itemsWithStock, sc))
+        }
     }
   }
 }
@@ -40,4 +47,12 @@ object Cafe extends App {
   case class OrderInvalidServiceCharge(message: String) extends CafeError(message)
 
   case class OrderInvalidItemList(message: String) extends CafeError(message)
+
+  val cafe = Cafe("Test Cafe", Menu(List(
+    Item("Test Item 0", 5.0, ColdDrink, 20),
+    Item("Test Item 1", 10.0, ColdFood, 5),
+    Item("Test Item 2", 20.0, HotFood, 5),
+    Item("Test Item 3", 30.0, PremiumMeal, 3)
+  )))
+
 }
