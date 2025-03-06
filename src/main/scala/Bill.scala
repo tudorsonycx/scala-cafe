2dd4e4be90d8c05fa931614e74b7facc2cff58af
tudorsonycx <tudorsonycx@gmail.com>
2025-03-06 10:28:21 +0000

    Refactor Bill class to separate subtotal and total calculation

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -1,16 +1,18 @@
 case class Bill(customer: Customer, items: Map[Item, Int], serviceCharge: Double) {
-  def total: Double = {
+  private def subTotal: Double = {
     val itemTotal = items.map({
       case (item, quantity) => item.price * quantity
     }).sum
     itemTotal
   }
 
+  def total: Double = subTotal * (1 + serviceCharge)
+
   override def toString: String = {
     val itemDetails = items.map({
       case (item, quantity) => s"${item.name} x $quantity"
     }).mkString("\n")
-    s"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $total\n" +
-      s"Service Charge: $serviceCharge\nTotal: ${total * (1 + serviceCharge)}"
+    s"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $subTotal\n" +
+      s"Service Charge: $serviceCharge\nTotal: $total"
   }
 }
