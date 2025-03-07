b743b9379319a3f88c27e84b7c50555272519da8
tudorsonycx <tudorsonycx@gmail.com>
2025-03-07 11:03:45 +0000

    Refactor Bill class to improve loyalty card discount logic and add discount handling for non-premium items; update tests accordingly

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -1,6 +1,6 @@
 case class Bill(customer: Customer, var items: Map[Item, Int], serviceCharge: Double) {
 
-  private def applyDrinksLoyaltyCardDiscount(): String = {
+  private def applyDrinksLoyaltyCardDiscount(): Unit = {
     customer.loyaltyCard match {
       case Some(card: DrinksLoyaltyCard) =>
         val firstDrink: Option[(Item, Int)] = items.find({
@@ -12,21 +12,33 @@ case class Bill(customer: Customer, var items: Map[Item, Int], serviceCharge: Do
 
         firstDrink match {
           case Some((drink, quantity)) =>
-            val message: String = if (card.isNextFree) {
+            if (card.isNextFree) {
               items = items + (drink -> (quantity - 1)) + (drink.copy(price = 0) -> 1)
-              s"Free drink applied: ${drink.name}"
-            } else {
-              s"Free drink not available yet"
             }
             card.addTimestamp()
-            message
-          case None => s"No drinks in order"
         }
-      case None => "No drinks loyalty card"
+      case _ => ()
     }
   }
 
-  applyDrinksLoyaltyCardDiscount()
+  private def applyDiscountLoyaltyCardDiscount(): Unit = {
+    if (subTotal >= 20) {
+      customer.loyaltyCard match {
+        case Some(card: DiscountLoyaltyCard) =>
+          items = items.map({
+            case (item, quantity) =>
+              val itemDiscount: Double = if (item.category != PremiumMeal) {
+                card.getStarCount * 0.02
+              } else 0
+
+              item.copy(price = item.price * (1 - itemDiscount)) -> quantity
+          })
+
+          card.addStar()
+        case _ => ()
+      }
+    }
+  }
 
   private def subTotal: Double = {
     val itemTotal = items.map({ case (item, quantity) => item.price * quantity }).sum
@@ -40,7 +52,11 @@ case class Bill(customer: Customer, var items: Map[Item, Int], serviceCharge: Do
     val itemDetails = items.map({
       case (item, quantity) => s"${item.name} x $quantity x ${item.price}"
     }).mkString("\n")
-    s"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $subTotal\n" +
-      s"Service Charge: $serviceCharge\nTotal: $total"
+    f"Customer: ${customer.name}\nItems:\n$itemDetails\nSubtotal: $subTotal%.2f\n" +
+      f"Service Charge: $serviceCharge\nTotal: $total%.2f"
   }
+
+  applyDrinksLoyaltyCardDiscount()
+
+  applyDiscountLoyaltyCardDiscount()
 }
--- src/test/scala/BillSpec.scala
+++ src/test/scala/BillSpec.scala
@@ -5,6 +5,19 @@ import java.time.LocalDate
 import scala.annotation.tailrec
 
 class BillSpec extends AnyWordSpec with Matchers {
+  final def getNTimestampsList(n: Int): List[LocalDate] = {
+    @tailrec
+    def iterate(i: Int = 0, acc: List[LocalDate] = List()): List[LocalDate] = {
+      if (i == n) {
+        acc
+      } else {
+        iterate(i + 1, acc :+ LocalDate.now().plusDays(n))
+      }
+    }
+
+    iterate()
+  }
+
   "Bill.toString" should {
     "return a string representation of the bill" in {
       val item0: Item = Item("Test Item 0", 5.0, ColdDrink, 20)
@@ -14,23 +27,18 @@ class BillSpec extends AnyWordSpec with Matchers {
       val serviceCharge: Double = 0.1
       val bill: Bill = Bill(customer, items, serviceCharge)
 
-      val expectedResult =
-        s"Customer: Test Customer\nItems:\nTest Item 0 x 2 x 5.0\nTest Item 1 x 1 x 10.0\nSubtotal: 20.0\nService Charge: 0.1\nTotal: 22.0"
+      val result: String = bill.toString
 
-      bill.toString shouldBe expectedResult
-    }
-
-    @tailrec
-    def get9TimestampsList(n: Int = 0, acc: List[LocalDate] = List()): List[LocalDate] = {
-      if (n == 9) {
-        acc
-      } else {
-        get9TimestampsList(n + 1, acc :+ LocalDate.now().plusDays(n))
-      }
+      result should include("Customer: Test Customer")
+      result should include("Test Item 0 x 2 x 5.0")
+      result should include("Test Item 1 x 1 x 10.0")
+      result should include("Subtotal: 20.0")
+      result should include(s"Service Charge: $serviceCharge")
+      result should include("Total: 22.0")
     }
 
     "return a string representation of the bill with drinks discount" in {
-      val timestampsMock: List[LocalDate] = get9TimestampsList()
+      val timestampsMock: List[LocalDate] = getNTimestampsList(9)
 
       val item0: Item = Item("Test Item 0", 5.0, ColdDrink, 20)
       val item1: Item = Item("Test Item 1", 10.0, ColdFood, 5)
@@ -45,14 +53,89 @@ class BillSpec extends AnyWordSpec with Matchers {
 
       val bill: Bill = Bill(customer, items, serviceCharge)
 
-      val expectedResult =
-        s"Customer: Test Customer\nItems:\nTest Item 0 x 1 x 5.0\nTest Item 1 x 1 x 10.0\n" +
-          "Test Item 0 x 1 x 0.0\nSubtotal: 15.0\nService Charge: 0.1\nTotal: 16.5"
+      val result: String = bill.toString
 
-      bill.toString shouldBe expectedResult
+      result should include("Customer: Test Customer")
+      result should include("Test Item 0 x 1 x 5.0")
+      result should include("Test Item 1 x 1 x 10.0")
+      result should include("Test Item 0 x 1 x 0.0")
+      result should include("Subtotal: 15.0")
+      result should include(s"Service Charge: $serviceCharge")
+      result should include("Total: 16.5")
+
       bill.customer.loyaltyCard match {
         case Some(card: DrinksLoyaltyCard) =>
           card.getTimestampsLength shouldBe 0
+      }
+    }
+
+    "return a string representation of the bill with discount for non-premium items" when {
+      "discount loyalty card has 1 star" in {
+        val item0: Item = Item("Test Item 0", 10.0, ColdFood)
+        val item1: Item = Item("Test Item 1", 6.0, HotDrink)
+        val item2: Item = Item("Test Item 2", 17.0, PremiumMeal)
+        val item3: Item = Item("Test Item 3", 5.0, AlcoholicDrink)
+
+        val customer: Customer = Customer("Test Customer", 18, Some(new DiscountLoyaltyCard() {
+          timestamps = getNTimestampsList(1)
+        }))
+
+        val items: Map[Item, Int] = Map(item0 -> 1, item1 -> 2, item2 -> 1, item3 -> 3)
+
+        val serviceCharge: Double = 0.25
+
+        val bill: Bill = Bill(customer, items, serviceCharge)
+
+        val result: String = bill.toString
+
+        result should include("Customer: Test Customer")
+        result should include("Test Item 0 x 1 x 9.8")
+        result should include("Test Item 1 x 2 x 5.88")
+        result should include("Test Item 2 x 1 x 17.0")
+        result should include("Test Item 3 x 3 x 4.9")
+        result should include("Subtotal: 53.26")
+        result should include(s"Service Charge: $serviceCharge")
+        result should include("Total: 66.58")
+
+        bill.customer.loyaltyCard match {
+          case Some(card: DiscountLoyaltyCard) =>
+            card.getStarCount shouldBe 2
+        }
+      }
+
+      "discount loyalty card has 8 stars" in {
+        val item0: Item = Item("Test Item 0", 20.0, HotFood)
+        val item1: Item = Item("Test Item 1", 30.0, PremiumMeal)
+        val item2: Item = Item("Test Item 2", 25.0, PremiumMeal)
+        val item3: Item = Item("Test Item 3", 5.0, AlcoholicDrink)
+        val item4: Item = Item("Test Item 4", 35.0, AlcoholicDrink)
+
+        val customer: Customer = Customer("Test Customer", 18, Some(new DiscountLoyaltyCard() {
+          timestamps = getNTimestampsList(8)
+        }))
+
+        val items: Map[Item, Int] = Map(item0 -> 2, item1 -> 1, item2 -> 2, item3 -> 10, item4 -> 2)
+
+        val serviceCharge: Double = 0.15
+
+        val bill: Bill = Bill(customer, items, serviceCharge)
+
+        val result: String = bill.toString
+
+        result should include("Customer: Test Customer")
+        result should include("Test Item 0 x 2 x 16.8")
+        result should include("Test Item 1 x 1 x 30.0")
+        result should include("Test Item 2 x 2 x 25.0")
+        result should include("Test Item 3 x 10 x 4.2")
+        result should include("Test Item 4 x 2 x 29.4")
+        result should include("Subtotal: 214.4")
+        result should include(s"Service Charge: $serviceCharge")
+        result should include("Total: 246.56")
+
+        bill.customer.loyaltyCard match {
+          case Some(card: DiscountLoyaltyCard) =>
+            card.getStarCount shouldBe 8
+        }
       }
     }
   }
